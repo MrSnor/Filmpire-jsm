511478b01a6791f1228a103ba7f6e5a6e3225f88
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-03-26 16:02:10 +0530

    add more to Movie View

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -5,6 +5,28 @@ export default makeStyles((theme) => ({
   movie: {
     padding: '10px',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+      transitionDuration: '500ms',
+
+    },
+  },
   title: {
     color: theme.palette.text.primary,
     textOverflow: 'ellipsis',
