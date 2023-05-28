2c06345a9e233a4c9908afaa403a117218f072d4
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-05-28 11:58:29 +0530

    add a basic error page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Typography, Button, Grid } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
 import { Route, Routes } from 'react-router-dom';
 import { Actors, MovieInformation, Profile, Movies, Navbar } from './index';
 
 import useStyles from './styles';
+
+function ErrorPage() {
+  return (
+    <Grid container alignItems="center" direction="column" gap={5} padding="6rem" border="5px red dotted">
+      <Typography variant="h1" align="center" color="error" fontFamily="Montserrat">
+        Error 404
+      </Typography>
+      <Button variant="contained" color="error" href="/" startIcon={<ArrowBack />} size="small">
+        Go back to home page
+      </Button>
+    </Grid>
+  );
+}
 
 function App() {
   const classes = useStyles();
@@ -20,6 +34,9 @@ function App() {
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/actors/:id" element={<Actors />} />
           <Route path="/profile/:id" element={<Profile />} />
+
+          {/* for all other routes, show a 404 page */}
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </main>
     </div>
