02298b431e6e9c2374ae28cb99f2c8fea54ae928
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-07-30 15:36:13 +0530

    update scrollbar

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 
 import App from './components/App';
 import store from './app/store';
-// import './index.css';
+import './index.css';
 
 const theme = createTheme({});
 
